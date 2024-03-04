f687a278535476c0f78384da2087eae3d057660b
Tamés <patriciotames@patricios-mini.speedport.ip>
2024-03-04 17:14:21 +0100

    things

--- index.js
+++ index.js
@@ -2,4 +2,46 @@ const express = require("express");
 const app = express();
 const port = process.env.port || 8080;
 
+require("dotenv").config();
+
+const { Pool, Client } = require("pg");
+
+const pool = new Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+});
+
+app.get("/", async (req, res) => {
+  try {
+    const data = await pool.query("SELECT * FROM users");
+    res.send(data.rows);
+  } catch (err) {
+    console.log(err.message);
+    res.sendStatus(500);
+  }
+});
+
+app.get("/:id", async (req, res) => {
+  try {
+    const data = await pool.query("SELECT * FROM users");
+    res.send(data.rows);
+  } catch (err) {
+    console.log(err.message);
+    res.sendStatus(500);
+  }
+});
+
+// app.get("/time", async (req, res) => {
+//   try {
+//     const data = await pool.query("SELECT NOW()");
+//     res.send(data);
+//   } catch (err) {
+//     console.log(err.message);
+//     res.sendStatus(500);
+//   }
+// });
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
