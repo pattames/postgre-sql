80463e88d9e6d7fd406c2f89fd3e1f8556671f65
Tamés <patriciotames@patricios-mini.speedport.ip>
2024-03-05 11:13:51 +0100

    final commit

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = process.env.port || 8080;
 require("dotenv").config();
 app.use(express.json());
 
+// const cors = require("cors");
+// app.use(cors());
+
 const { Pool, Client } = require("pg");
 
 const pool = new Pool({
@@ -16,7 +19,7 @@ const pool = new Pool({
 });
 
 //Dame a todos los usuarios
-app.get("/", async (req, res) => {
+app.get("/API/users", async (req, res) => {
   try {
     const data = await pool.query("SELECT * FROM users");
     res.send(data.rows);
@@ -27,7 +30,7 @@ app.get("/", async (req, res) => {
 });
 
 //Dame un usuario basado en su id
-app.get("/:id", async (req, res) => {
+app.get("/API/users/:id", async (req, res) => {
   const userID = Number(req.params.id);
 
   try {
@@ -42,7 +45,7 @@ app.get("/:id", async (req, res) => {
 });
 
 //Crea a un nuevo ususario
-app.post("/", async (req, res) => {
+app.post("/API/users", async (req, res) => {
   const { id, first_name, last_name, age, active } = req.body;
 
   try {
@@ -58,7 +61,7 @@ app.post("/", async (req, res) => {
 });
 
 //Edita un ususario existente
-app.put("/:id", async (req, res) => {
+app.put("/API/users/:id", async (req, res) => {
   const userID = Number(req.params.id);
   const { first_name, last_name, age, active } = req.body;
 
@@ -75,12 +78,85 @@ app.put("/:id", async (req, res) => {
 });
 
 //Borra a un usuario
-app.delete("/:id", async (req, res) => {
+app.delete("/API/users/:id", async (req, res) => {
   const userID = Number(req.params.id);
 
   try {
+    // Delete associated orders first
+    await pool.query("DELETE FROM orders WHERE user_id = $1", [userID]);
+    //Luego borra al ususario
     await pool.query("DELETE FROM users WHERE id = $1", [userID]);
     res.send("User deleted");
+  } catch (err) {
+    console.log(err.message);
+    res.sendStatus(500);
+  }
+});
+
+//Dame todas los órdenes
+app.get("/API/orders", async (req, res) => {
+  try {
+    const data = await pool.query("SELECT * FROM orders");
+    res.send(data.rows);
+  } catch (err) {
+    console.log(err.message);
+    res.sendStatus(500);
+  }
+});
+
+//Dame una sola orden
+app.get("/API/orders/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const data = await pool.query("SELECT * FROM orders WHERE id = $1", [id]);
+    res.send(data.rows);
+  } catch (err) {
+    console.log(err.message);
+    res.sendStatus(500);
+  }
+});
+
+//Crea a un nuevo ususario
+app.post("/API/orders", async (req, res) => {
+  const { price, date, user_id } = req.body;
+
+  try {
+    const data = await pool.query(
+      "INSERT INTO orders (price, date, user_id) VALUES ($1, $2, $3) RETURNING *",
+      [price, date, user_id]
+    );
+    res.send(data.rows);
+  } catch (err) {
+    console.log(err.message);
+    res.sendStatus(500);
+  }
+});
+
+//Edit one order
+app.put("/API/orders/:id", async (req, res) => {
+  const { id } = req.params;
+  const { price, date, user_id } = req.body;
+
+  try {
+    const data = await pool.query(
+      "UPDATE orders SET price = $1, date = $2, user_id = $3 WHERE id = $4 RETURNING *",
+      [price, date, user_id, id]
+    );
+    res.json(data.rows);
+  } catch (err) {
+    console.log(err.message);
+    res.sendStatus(500);
+  }
+});
+
+//Delete an order
+app.delete("/API/orders/:id", async (req, res) => {
+  const orderID = Number(req.params.id);
+
+  try {
+    await pool.query("DELETE FROM orders WHERE id = $1", [orderID]);
+    res.send("Order deleted");
   } catch (err) {
     console.log(err.message);
     res.sendStatus(500);
